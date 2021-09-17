f67a1b6365abae9b47d6f9e7f463c254ad8f5ff6
Fabricio Sena <fabricior.sena@outlook.com>
2021-09-17 00:46:14 -0300

    refactoring to get and press prime number

--- FrameworkTest/Program.cs
+++ FrameworkTest/Program.cs
@@ -7,22 +7,50 @@ namespace FrameworkTest
     {
         static void Main(string[] args)
         {
+            List<int> denominators = new List<int>();
+            List<int> primeNumbers = new List<int>();
+
             Console.Write("Digite um número: ");
             int number = int.Parse(Console.ReadLine());
 
-            List<int> factors = new List<int>();
+            denominators = HandleDenominatorsBySpecificInt(number, denominators);
+            primeNumbers = HandlePrimeNumbersByDenominators(denominators, primeNumbers);
 
+            string outputDenominators = ConvertListToString(" ", denominators);
+            string outputPrimeNumbers = ConvertListToString(" ", primeNumbers);
+
+            Console.WriteLine("Números divisores: " + outputDenominators);
+            Console.WriteLine("Números primos: " + outputPrimeNumbers);
+        }
+
+        private static string ConvertListToString(string separator, List<int> list)
+        {
+            return string.Join(separator, list.ToArray());
+        }
+
+        private static List<int> HandleDenominatorsBySpecificInt(int number, List<int> denominators)
+        {
             for (int i = 1; i <= number; i++)
             {
-                int result = number % i;
-                if (result == 0)
-                    factors.Add(i);
-
-                Console.WriteLine("Result:" + result);
+                if (number % i == 0)
+                    denominators.Add(i);
             }
-
-            string outputFacotrs = string.Join(" ", factors.ToArray());
-            Console.WriteLine("Números divisores: " + outputFacotrs);
+            return denominators;
+        }
+        private static List<int> HandlePrimeNumbersByDenominators(List<int> denominators, List<int> primeNumbers)
+        {
+            foreach (int denominator in denominators)
+            {
+                int count = 0;
+                for (int i = denominator; i > 0; i--)
+                {
+                    if (denominator % i == 0)
+                        count++;
+                }
+                if (count == 2)
+                    primeNumbers.Add(denominator);
+            }
+            return primeNumbers;
         }
     }
 }
