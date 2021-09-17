8fa4ec7b99862578701aa7230d0502d5ff211053
Fabricio Sena <fabricior.sena@outlook.com>
2021-09-16 23:10:36 -0300

    add find factors of a number given

--- FrameworkTest/Program.cs
+++ FrameworkTest/Program.cs
@@ -7,7 +7,22 @@ namespace FrameworkTest
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Digite um número: ");
+            int number = int.Parse(Console.ReadLine());
+
+            List<int> factors = new List<int>();
+
+            for (int i = 1; i <= number; i++)
+            {
+                int result = number % i;
+                if (result == 0)
+                    factors.Add(i);
+
+                Console.WriteLine("Result:" + result);
+            }
+
+            string outputFacotrs = string.Join(" ", factors.ToArray());
+            Console.WriteLine("Números divisores: " + outputFacotrs);
         }
     }
 }
